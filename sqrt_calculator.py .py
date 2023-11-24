3f9fca7b134ea6996efe3c5aa46b020f5dc481ef
zainabyauibrahim <151638614+zainabyauibrahim@users.noreply.github.com>
2023-11-24 18:54:54 +0100

    Add files via upload

---
+++ sqrt_calculator.py .py
@@ -0,0 +1,12 @@
+num = 8
+num = float(input('Enter a number:'))
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num,num_sqrt))
+
+
+
+
+num = float(input('Enter a number:'))
+sqrt_root= num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num,sqrt_root))
+
