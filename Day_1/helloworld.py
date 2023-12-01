9a25fb6fac8099c7483d0a0b4cf92912f9548255
zainabyauibrahim <151638614+zainabyauibrahim@users.noreply.github.com>
2023-12-01 14:46:16 +0100

    helloworld.py

--- Day_1/helloworld.py
+++ Day_1/helloworld.py
@@ -3,3 +3,33 @@ print("use this code in terminal \n 'python --version'")
 
 print(3+4)
 print(4-3)
+print(3*4)
+print(3%4)
+print(3/4)   
+print(3**4) 
+print(3//4)
+
+'Zainab'
+'Ibrahim'
+'Nigeria'
+'I am enjoying 30 days of python'
+'Number integer'
+'Number float'
+'Number float'
+'Number complex'
+'List'
+'String'
+'String'
+'String'
+'Exercise: Level 3'
+print(type(10)) Integer
+print(type(3.14)) Float
+print(type(1+3j)) Complex
+print(type('Asabe')) SyntaxWarning
+print(type[1, 2, 3]) List
+print(type({'name':'Zainab'})) Dictionary
+print(type({9.8, 3.4, 2.15})) Set
+print(type((8.6, 6.8, 4.33))) Tuple
+
+
+
